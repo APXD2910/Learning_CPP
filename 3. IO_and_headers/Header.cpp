6fae13a4680936311218b0851254b59d0441536f
Maulik Shah <maulik.shah715@gmail.com>
2020-10-26 21:41:57 +0530

    Learnt about operators and input/output

---
+++ 3. IO_and_headers/Header.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+// If we use Ctrl+click on header, it will open
+
+// There are 2 tpyes of header files
+// 1) System Header Files: It comes automatically with the compiler
+// For searching for headers: https://en.cppreference.com/w/cpp/header 
+
+// 2) User Defined Header: It is written by the programmer like this
+
+// #include"This.h"   => This will produce an error because this is not on the directory
+
+using namespace std;
+
+int main(){
+    cout << "This is a simple Hello World Program"
+    return 0;
+}
