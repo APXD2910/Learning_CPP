6cab88ffb29de7b2c8d4f79cb0b35d80b44b73c8
Maulik Shah <maulik.shah715@gmail.com>
2020-10-27 14:46:30 +0530

    Fixed a bug that was stopping me from compiling my program

--- 3. IO_and_headers/Input_and_output.cpp
+++ 3. IO_and_headers/Input_and_output.cpp
@@ -5,10 +5,10 @@ int main()
 {
     int num1, num2;
     cout<<"Enter the value if First number:\n";/*This is called Insertion operator*/
-    cin<<num1;/*This is called Insertion operator*/
+    cin>>num1;/*This is called Extraction operator*/
 
     cout<<"Enter the value if second number:\n";
-    cin<<num2;
+    cin>>num2;
 
     cout<<"The sum of the 2 numbers is: "<<num1+num2;
     return 0;
---
+++ 4. Ref_variables_and_constants/RefVariables.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "This program takes 2 numbers and returns their sum" << endl;
+    cout<<"Enter the first number : "<<endl;
+    cin>>a;
+    cout<<"Enter the second number: "<<endl;
+    cin>>b;
+    cout<<"The sum is "<<a+b;
+    return 0;
+}
