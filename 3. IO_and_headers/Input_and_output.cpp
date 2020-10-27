bb6d4596eaa9c2c3bd53badbf16ce29a5bdbe739
Maulik Shah <maulik.shah715@gmail.com>
2020-10-27 14:50:52 +0530

    Fixed some typos

--- 3. IO_and_headers/Input_and_output.cpp
+++ 3. IO_and_headers/Input_and_output.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
     int num1, num2;
-    cout<<"Enter the value if First number:\n";/*This is called Insertion operator*/
+    cout<<"Enter the value of First number:\n";/*This is called Insertion operator*/
     cin>>num1;/*This is called Extraction operator*/
 
-    cout<<"Enter the value if second number:\n";
+    cout<<"Enter the value of second number:\n";
     cin>>num2;
 
     cout<<"The sum of the 2 numbers is: "<<num1+num2;
