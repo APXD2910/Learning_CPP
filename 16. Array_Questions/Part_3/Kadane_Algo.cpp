0862ff5a2740e6450f450e0f2e438dc55191e0af
Maulik Shah <maulik.shah715@gmail.com>
2020-11-08 22:39:19 +0530

    Learnt Kadane's Algorithm

---
+++ 16. Array_Questions/Part_3/Kadane_Algo.cpp
@@ -0,0 +1,26 @@
+// In this we will use Kadane's algorithm
+// The time complexity of this is O(n)
+// This is extremely new for me
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int currentSum=0;
+    int maxSum = INT_MIN;
+    for(int i=0; i<n; i++){
+        currentSum += arr[i];
+        if(currentSum < 0){
+            currentSum = 0;
+        }
+        maxSum = max(maxSum, currentSum);
+    }
+    cout << maxSum << "\n";
+    return 0;
+}
