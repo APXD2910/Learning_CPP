b288bb917661e5559d679f383d72b342d183f82e
Maulik Shah <maulik.shah715@gmail.com>
2020-11-09 18:03:53 +0530

    Learnt about constants

---
+++ 4. Ref_variables_and_constants/Constants.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	const int x = 23;
+	cout << "Constants are basically vriables whose values cannot ever be changed\n";
+	cout << "Constants are generally represents will ALL_CAPS names to seperate them"<<
+	" from variables\n";
+	
+	cout << "For example, " << x << " is a constant\n";
+	cout << "It is quite a basic concept\n";
+	return 0;
+}
