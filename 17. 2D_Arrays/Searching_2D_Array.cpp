4cb6579bc2397518d2e4b937d4b2024affe794c9
Maulik Shah <maulik.shah715@gmail.com>
2020-11-11 08:57:02 +0530

    Rearranged the notes

---
+++ 17. 2D_Arrays/Searching_2D_Array.cpp
@@ -0,0 +1,15 @@
+// This is very similar to matrix in maths
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    int arr[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> arr[i][j];
+        }
+    }
+    return 0;
+}
