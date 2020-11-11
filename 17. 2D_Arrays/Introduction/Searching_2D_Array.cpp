52bc41ed6f26c23f24d9a4b90ea47c072eb13517
Maulik Shah <maulik.shah715@gmail.com>
2020-11-11 11:16:19 +0530

    Learnt basic of 2D arrays

---
+++ 17. 2D_Arrays/Introduction/Searching_2D_Array.cpp
@@ -0,0 +1,30 @@
+// This is very similar to matrix in maths
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    int arr[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    int x;
+    cin >> x;
+    bool flag=false;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(arr[i][j]==x){
+                cout << i << " " << j << endl;
+                flag=true;
+            }
+        }
+    }
+    if(flag==false){
+        cout << "Element not found\n";
+    }
+    return 0;
+}
